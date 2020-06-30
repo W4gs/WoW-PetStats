9deaf5f3ca4e77c00a1977cc83ce2b164bb3dbbb
Wags <daniel.wagnerjr@gmail.com>
2020-06-30 15:23:15 -0400

    Update PetStats.lua

--- PetStats.lua
+++ PetStats.lua
@@ -12,13 +12,16 @@ function()
         return "";
     end
     
+    local petApBase, petApPos, petApNeg = UnitAttackPower("pet");
+    local petAdLow, petAdHigh, _, _, petAdPos, petAdNeg, petAdPerc = UnitDamage("pet");
+    
     tblPlayerStats = { }
     tblPlayerStats["ID"] = classID;
     tblPlayerStats["Class"] = playerClass;
     
     tblPetStats = { }
-    tblPetStats["AttackSpeed"] = UnitAttackSpeed("pet");
-    tblPetStats["AttackPower"] = UnitAttackPower("pet");
+    tblPetStats["AttackSpeed"] = select(1, UnitAttackSpeed("pet"));
+    tblPetStats["AttackPower"] = petApBase + petApPos + petApNeg;
     tblPetStats["AttackDamage"] = UnitDamage("pet");
     tblPetStats["PetInRange"] = UnitInRange("pet");
     tblPetStats["PetArmor"] = UnitArmor("pet");
